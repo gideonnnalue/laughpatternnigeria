a4222648c1d46656caab6e4757ae1baff9e4b7e5
Gideon Nnalue <emekannalue@gmail.com>
2019-07-05 08:14:47 -0400

    added scroll to top

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
+import ScrollToTop from "./ScrollToTop";
+
+// styling
 import "bootstrap/dist/css/bootstrap.min.css";
 import $ from "jquery";
 import Popper from "popper.js";
@@ -8,6 +11,8 @@ import "bootstrap/dist/js/bootstrap.bundle.min";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./assets/styles/sass/styles.scss";
+
+// Component Pages
 import Navigation from "./components/navigation";
 import Footer from "./components/footer";
 import Landing from "./components/mainPage/landing";
@@ -20,113 +25,142 @@ import SingleEvent from "./components/mainPage/singleEvent";
 import SingleNewsFeed from "./components/mainPage/singleNewsFeed";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      navShow: false
-    };
+    constructor(props) {
+        super(props);
+        this.state = {
+            navShow: false
+        };
 
-    this.showNav = this.showNav.bind(this);
-    this.turnOffNav = this.turnOffNav.bind(this);
-  }
-
-  componentDidMount() {
-    const ele = document.getElementById("ipl-progress-indicator");
-    if (ele) {
-      // fade out
-      ele.classList.add("available");
-      setTimeout(() => {
-        // remove from DOM
-        ele.outerHTML = "";
-      }, 2000);
+        this.showNav = this.showNav.bind(this);
+        this.turnOffNav = this.turnOffNav.bind(this);
     }
-    // this.authenticate().then(() => {
-    //   const ele = document.getElementById('ipl-progress-indicator');
-    //   if(ele){
-    //     // fade out
-    //     ele.classList.add('available')
-    //     setTimeout(() => {
-    //       // remove from DOM
-    //       ele.outerHTML = ''
-    //     }, 2000)
-    //   }
-    // })
-  }
 
-  authenticate() {
-    return new Promise(resolve => setTimeout(resolve, 2000));
-  }
+    componentDidMount() {
+        const ele = document.getElementById("ipl-progress-indicator");
+        if (ele) {
+            // fade out
+            ele.classList.add("available");
+            setTimeout(() => {
+                // remove from DOM
+                ele.outerHTML = "";
+            }, 2000);
+        }
+        // this.authenticate().then(() => {
+        //   const ele = document.getElementById('ipl-progress-indicator');
+        //   if(ele){
+        //     // fade out
+        //     ele.classList.add('available')
+        //     setTimeout(() => {
+        //       // remove from DOM
+        //       ele.outerHTML = ''
+        //     }, 2000)
+        //   }
+        // })
+    }
 
-  showNav() {
-    this.setState({ navShow: !this.state.navShow });
-  }
+    authenticate() {
+        return new Promise(resolve => setTimeout(resolve, 2000));
+    }
 
-  turnOffNav() {
-    this.setState({ navShow: false });
-  }
+    showNav() {
+        this.setState({ navShow: !this.state.navShow });
+    }
 
-  render() {
-    return (
-      <Router>
-        <div className="App">
-          <Navigation showNav={this.showNav} navShow={this.state.navShow} />
-          <Route
-            path="/"
-            exact
-            render={routeProps => (
-              <Landing {...routeProps} turnOffNav={this.turnOffNav} />
-            )}
-          />
-          <Route
-            path="/about"
-            render={routeProps => (
-              <About {...routeProps} turnOffNav={this.turnOffNav} />
-            )}
-          />
-          <Route
-            path="/events"
-            render={routeProps => (
-              <Events {...routeProps} turnOffNav={this.turnOffNav} />
-            )}
-          />
-          <Route
-            path="/news"
-            render={routeProps => (
-              <NewsFeed {...routeProps} turnOffNav={this.turnOffNav} />
-            )}
-          />
-          <Route
-            path="/gallery"
-            render={routeProps => (
-              <Gallery {...routeProps} turnOffNav={this.turnOffNav} />
-            )}
-          />
-          <Route
-            path="/members"
-            render={routeProps => (
-              <Members {...routeProps} turnOffNav={this.turnOffNav} />
-            )}
-          />
-          <Route
-            path="/event/:eventID"
-            render={routeProps => (
-              <SingleEvent {...routeProps} turnOffNav={this.turnOffNav} />
-            )}
-          />
+    turnOffNav() {
+        this.setState({ navShow: false });
+    }
 
-          <Route
-            path="/newsfeed/:newsfeedID"
-            render={routeProps => (
-              <SingleNewsFeed {...routeProps} turnOffNav={this.turnOffNav} />
-            )}
-          />
-          {/* <Route exact path="/" component={Landing} /> */}
-          {/* <Route exact path="/about" component={About} /> */}
-          <Footer />
-        </div>
-      </Router>
-    );
-  }
+    render() {
+        return (
+            <Router>
+                <ScrollToTop>
+                    <div className="App">
+                        <Navigation
+                            showNav={this.showNav}
+                            navShow={this.state.navShow}
+                        />
+                        <Route
+                            path="/"
+                            exact
+                            render={routeProps => (
+                                <Landing
+                                    {...routeProps}
+                                    turnOffNav={this.turnOffNav}
+                                />
+                            )}
+                        />
+                        <Route
+                            path="/about"
+                            render={routeProps => (
+                                <About
+                                    {...routeProps}
+                                    turnOffNav={this.turnOffNav}
+                                />
+                            )}
+                        />
+                        <Route
+                            path="/events"
+                            render={routeProps => (
+                                <Events
+                                    {...routeProps}
+                                    turnOffNav={this.turnOffNav}
+                                />
+                            )}
+                        />
+                        <Route
+                            path="/news"
+                            render={routeProps => (
+                                <NewsFeed
+                                    {...routeProps}
+                                    turnOffNav={this.turnOffNav}
+                                />
+                            )}
+                        />
+                        <Route
+                            path="/gallery"
+                            render={routeProps => (
+                                <Gallery
+                                    {...routeProps}
+                                    turnOffNav={this.turnOffNav}
+                                />
+                            )}
+                        />
+                        <Route
+                            path="/members"
+                            render={routeProps => (
+                                <Members
+                                    {...routeProps}
+                                    turnOffNav={this.turnOffNav}
+                                />
+                            )}
+                        />
+                        <Route
+                            path="/event/:eventID"
+                            render={routeProps => (
+                                <SingleEvent
+                                    {...routeProps}
+                                    turnOffNav={this.turnOffNav}
+                                />
+                            )}
+                        />
+
+                        <Route
+                            path="/newsfeed/:newsfeedID"
+                            render={routeProps => (
+                                <SingleNewsFeed
+                                    {...routeProps}
+                                    turnOffNav={this.turnOffNav}
+                                />
+                            )}
+                        />
+                        {/* <Route exact path="/" component={Landing} /> */}
+                        {/* <Route exact path="/about" component={About} /> */}
+                        <Footer />
+                    </div>
+                </ScrollToTop>
+            </Router>
+        );
+    }
 }
 
 export default App;
