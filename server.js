2edced0788fb6ad89135e98a525f6f8e945cf604
Gideon Nnalue <emekannalue@gmail.com>
2019-07-12 23:25:23 -0400

    set for heroku

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 
+const path = require("path");
+
 const users = require("./routes/api/users");
 const events = require("./routes/api/events");
 const members = require("./routes/api/members");
@@ -37,6 +39,16 @@ require("./config/passport")(passport);
 app.use("/api/users", users);
 app.use("/api/events", events);
 app.use("/api/members", members);
+
+// Server static assets if in production
+if (process.env.NODE_ENV === "production") {
+    // Set static folder
+    app.use(express.static("client/build"));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+}
 
 const port = process.env.PORT || 5000;
 
