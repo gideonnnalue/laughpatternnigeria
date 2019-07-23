d125b12840d340b54c8733672c432ee55ddca59a
Gideon Nnalue <emekannalue@gmail.com>
2019-07-22 21:53:47 -0400

    added build

--- server.js
+++ server.js
@@ -60,6 +60,6 @@ app.get("*", (req, res) => {
 //     });
 // }
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
